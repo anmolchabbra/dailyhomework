8b16618bda38e708ecfddf1e6b15af520a38a81b
Anmol Chabbra <anmolchabbra1@gmail.com>
2019-01-16 22:48:35 +0530

    my 2nd homework

--- src/D20190113.java
+++ src/D20190113.java
@@ -1,5 +1,14 @@
 public class D20190113 {
     public static void main(String[] args) {
-
+        D20190113.tombbradyage();
+    }
+    static void tombbradyage() {
+        int tombbradyage = 51;
+        if (tombbradyage >= 50) {
+            System.out.println("he should retire");
+        } else {
+            System.out.println("he can still beat");
+        }
     }
 }
+
