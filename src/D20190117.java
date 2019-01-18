0bab5e1e20d5e45ffd6ff16d1fe8167efa16d1e8
Anmol Chabbra <anmolchabbra1@gmail.com>
2019-01-18 17:11:49 +0530

    my second commit

---
+++ src/D20190117.java
@@ -0,0 +1,8 @@
+public class D20190117 {
+    public static void main (String[] args) {
+        int count = 8;
+        double temperature = 34.3;
+        char letter = 'a';
+        boolean isCS125Awesome = true;
+    }
+}
